af6247f91320c03dd6301a77f181dabbec37e1fb
José Alberto Ruiz <jose-alberto.ruiz@sopra.com>
2016-10-17 22:38:19 +0200

    Initial import

---
+++ spring-chapter01/src/test/java/com/jaruiz/springtutorial/chapter01/SpringChapter01ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jaruiz.springtutorial.chapter01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringChapter01ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
